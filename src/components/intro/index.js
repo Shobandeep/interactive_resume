cee62693308d57493f3cb3b79be829329223887b
shobandeep@gmail.com <Beast213>
2020-11-30 17:28:04 -0500

    stuff

---
+++ src/components/intro/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './intro.css';
+
+
+export default function Home() {
+  return (
+    <div></div>
+  )
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React from "react";
 
-import Header from '../components/header'
+import Header from '../components/header';
+import './global.css';
 
 export default function Home() {
   return (
-    <Header />
+    <html lang="en">
 
+      <head>
+          <meta charset="UTF-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <link rel="stylesheet" href="styles.css"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet"/> 
+          <title>Hire this guy</title>
+      </head>
+
+      <body>
+        <Header />
+
+      </body>
+    </html>
   )
-}
+};
