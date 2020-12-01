a9577d8d138650bba27ce93e9e3b9a52c4cb8f80
shobandeep@gmail.com <Beast213>
2020-12-01 11:25:28 -0500

    css santitized

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -4,6 +4,11 @@ import './intro.css';
 
 export default function Home() {
   return (
-    <div></div>
+    <div class="intro page">
+
+        <div>John Doe</div>
+        <div class="job-title flip">Job Title</div>
+        
+    </div>
   )
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import Header from '../components/header';
 import './global.css';
+import Header from '../components/header';
+import Intro from '../components/intro';
+
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
       <head>
           <meta charset="UTF-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <link href="https://unpkg.com/sanitize.css" rel="stylesheet" />
           <link rel="stylesheet" href="styles.css"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet"/> 
@@ -18,6 +21,7 @@ export default function Home() {
 
       <body>
         <Header />
+        <Intro />
 
       </body>
     </html>
