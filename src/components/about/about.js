0d9f317800ce6243c32d38c866383914b6ca9559
shobandeep@gmail.com <Beast213>
2020-12-01 14:51:01 -0500

    about page started

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,13 +1,28 @@
 import React from "react";
-import './intro.scss';
+import './about.scss';
 
 
 export default function Home() {
   return (
-    <div id="intro" class="intro page">
+    <div id="about" class="about">
 
-        <div>John Doe</div>
-        <div class="job-title flip">Job Title</div>
+        <div class="about-title">My journey</div>
+
+        <div class="about-body">
+          <div class="about-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies dolor eu scelerisque tempor. Pellentesque mauris massa, ultrices vel vulputate at, 
+            egestas id enim. Phasellus eu est eget nisl tempus dignissim vitae ut magna. Integer ullamcorper pretium faucibus. Aenean rhoncus urna ac consectetur vestibulum. 
+            Fusce imperdiet condimentum purus eget suscipit. Donec est nisi, dapibus tempor nibh id, sollicitudin cursus lectus. Sed odio eros, consequat quis vulputate non, 
+            rutrum nec eros. Etiam eget sem metus. Nam volutpat diam et mollis mattis. Sed pellentesque vulputate dolor sit amet porttitor. Ut ultricies lectus ut velit tincidunt, a auctor ligula elementum.
+            Maecenas consequat nunc eu lectus suscipit mollis. Donec vitae luctus turpis, et porttitor felis. Duis venenatis posuere pharetra. Vivamus euismod lorem sit 
+            amet magna rutrum finibus. Pellentesque auctor, nulla id feugiat imperdiet, diam risus consectetur lacus, quis ultrices eros lorem ut nulla. 
+            Sed blandit dolor sit amet nulla posuere congue. Vestibulum sagittis a erat at luctus. Duis aliquam iaculis eros quis molestie. Sed arcu mauris, 
+            facilisis sed magna quis, hendrerit dignissim felis. Ut a mauris porta, porta leo at, consequat enim. Fusce sit amet dolor sollicitudin, cursus sapien eget, 
+            sagittis libero. Vestibulum vitae eros et turpis pharetra semper eget ac neque. 
+          </div>
+
+          <img class="body-img" src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg" />
+        </div>
         
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,28 +3,15 @@ import React from "react";
 import './global.scss';
 import Header from '../components/header/header';
 import Intro from '../components/intro/intro';
+import About from '../components/about/about';
 
 
 export default function Home() {
   return (
-    <html lang="en">
-
-      <head>
-          <meta charset="UTF-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <link href="https://unpkg.com/sanitize.css" rel="stylesheet" />
-          <link rel="stylesheet" href="styles.css"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet"/> 
-          <title>Hire this guy</title>
-      </head>
-
-      <body>
+    <React.Fragment>
         <Header />
         <Intro />
-        <Intro />
-
-      </body>
-    </html>
+        <About />
+    </React.Fragment>
   )
 };
