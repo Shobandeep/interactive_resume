0eb1e77f2bb3f29caa745c5045354d3bbfa7e05f
funk <shobandeep@gmail.com>
2020-12-05 19:26:58 -0500

    animations added

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './about.scss';
 
+import ScrollReveal from 'scrollreveal';
+
+const scrollReveal = new ScrollReveal();
+
+const srConfig = (delay = 200, viewFactor = 0.25) => ({
+  origin: 'bottom',
+  distance: '20px',
+  duration: 500,
+  delay,
+  rotate: { x: 0, y: 0, z: 0 },
+  opacity: 0,
+  scale: 1,
+  easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  mobile: true,
+  reset: false,
+  useDelay: 'always',
+  viewFactor,
+})
 
 export default function Home() {
+
+  const ref = React.useRef(null);
+  const ref2 = React.useRef(null);
+
+
+  React.useEffect(() => scrollReveal.reveal(ref.current, srConfig()) ,[]);
+  React.useEffect(() => scrollReveal.reveal(ref2.current, srConfig()) ,[]);
+
+
   return (
-    <div id="about" class="about">
+    <div id="about" class="about" ref={ref}>
 
-        <div class="about-title">My journey</div>
+        <div class="about-title slide-in-left" >My journey</div>
 
-        <div class="about-body">
+        <div class="about-body" ref={ref2}>
+
           <div class="about-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies dolor eu scelerisque tempor. Pellentesque mauris massa, ultrices vel vulputate at, 
             egestas id enim. Phasellus eu est eget nisl tempus dignissim vitae ut magna. Integer ullamcorper pretium faucibus. Aenean rhoncus urna ac consectetur vestibulum. 
             Fusce imperdiet condimentum purus eget suscipit. Donec est nisi, dapibus tempor nibh id, sollicitudin cursus lectus. Sed odio eros, consequat quis vulputate non, 
             rutrum nec eros. Etiam eget sem metus. Nam volutpat diam et mollis mattis. Sed pellentesque vulputate dolor sit amet porttitor. Ut ultricies lectus ut velit tincidunt, a auctor ligula elementum.
-            Maecenas consequat nunc eu lectus suscipit mollis. Donec vitae luctus turpis, et porttitor felis. Duis venenatis posuere pharetra. Vivamus euismod lorem sit 
-            amet magna rutrum finibus. Pellentesque auctor, nulla id feugiat imperdiet, diam risus consectetur lacus, quis ultrices eros lorem ut nulla. 
-            Sed blandit dolor sit amet nulla posuere congue. Vestibulum sagittis a erat at luctus. Duis aliquam iaculis eros quis molestie. Sed arcu mauris, 
-            facilisis sed magna quis, hendrerit dignissim felis. Ut a mauris porta, porta leo at, consequat enim. Fusce sit amet dolor sollicitudin, cursus sapien eget, 
-            sagittis libero. Vestibulum vitae eros et turpis pharetra semper eget ac neque. 
           </div>
 
           <img class="body-img" src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"  alt="an image of me!" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,10 @@ import Footer from '../components/footer/footer';
 export default function Home() {
   return (
     <React.Fragment>
+      
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet" /> 
+        <link href="https://unpkg.com/sanitize.css" rel="stylesheet" />
 
         <Header />
         <Intro />
